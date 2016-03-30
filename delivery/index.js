26937da93f7e91af1698a512a78fa789268ec2cf
Ekaterina Lizogubova <lizogubova@wookieelabs.com>
2016-03-31 00:34:29 +0300

    Add order schemas

--- delivery/index.js
+++ delivery/index.js
@@ -1,8 +1,8 @@
 var soap = require('soap');
-var url = 'http://andrei.xianet.com.ua/public/delivering.php?r=ticket';
+var url = 'http://andrei.xianet.com.ua/public/storage.php?r=item';
 
 soap.createClient(url, function(err, client) {
-  client.getAllTickets({order_id: null, address: null, courier_id: null}, function(err, result) {
-    console.log(result.return);
+  client.getAllItems({product_id: null, storage_id: null, status_id: null}, function(err, result) {
+    console.log(result.return.item);
   });
 });
